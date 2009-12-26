1d4ba5b573f425582c057b7fa7481d176afccf63
Keisuke Hashimoto (id:khashi) <ksk.hashi@gmail.com>
2009-12-26 21:38:58 +0900

    *add つぶやきToolbarを表示するtsubuyakiToolbarを追加。絵文字テーブルから絵文字を選択できるようになる。

--- haiku2utils.user.js
+++ haiku2utils.user.js
@@ -2,7 +2,7 @@
 // @name           Haiku2Utils
 // @namespace      http://www.scrapcode.net/
 // @include        http://h2.hatena.ne.jp/*
-// @version        0.0.16.1
+// @version        0.0.17
 // ==/UserScript==
 (function( uWindow ) {
     // Select utility
@@ -42,6 +42,9 @@
 
         // うごメモのインラインプレーヤー化
         { name: 'ugomemoPlayer', args: {} },
+
+        // つぶやきToolbar表示
+        { name: 'tsubuyakiToolbar', args: {} },
     ];
 
     const ID_REGEXP = '[a-zA-Z][a-zA-Z0-9_-]{1,30}[a-zA-Z0-9]';
@@ -116,17 +119,22 @@
 
         for( var i = 0; i < inputs.length; ++i ) {
             var input = inputs[i];
-            var tarea = createElement( 'textarea', {
-                name: 'body',
+            var div   = createElement( 'div', {
+                id: 'h2u_bodyBox',
             }, {
-                width:        '60%',
-                height:       '70px',
-                padding:      '3px',
-                display:      'block',
+                width: '60%',
                 marginBottom: '5px',
             } );
+            div.appendChild( createElement( 'textarea', {
+                name: 'body',
+                id:   'h2u_body',
+            }, {
+                width:   '100%',
+                height:  '70px',
+                padding: '3px',
+            } ) );
 
-            input.form.replaceChild( tarea, input );
+            input.parentNode.replaceChild( div, input );
         }
     };
 
@@ -393,9 +401,116 @@
         }
     };
 
+    utils.tsubuyakiToolbar = function ( args ) {
+        var textbox = document.getElementsByName( 'body' );
+        var toolbar = createElement( 'div', {
+            id: 'h2u_toolbar',
+        }, {
+            textAlign: 'left',
+        } );
+
+        for( var i = 0; i < textbox.length; ++i ) {
+            var tbox = textbox[i];
+            var tag  = tbox.tagName.toLowerCase();
+            if( tag != 'textarea' && tag != 'input' && tbox.type != 'text' ) continue;
+            tbox.parentNode.insertBefore( toolbar, tbox );
+            new EmojiTable( toolbar, tbox );
+        }
+    }
+
     for( var i = 0; i < runUtils.length; ++i ) {
         var target = runUtils[i];
         utils[ target.name ]( target.args );
+    }
+
+    function emojiImage( code, style, event ) {
+        if( ! style ) style = {};
+        if( ! event ) event = {};
+
+        return img = createElement( 'img', {
+            src: 'http://ugomemo.hatena.ne.jp/images/emoji/e-' + code + '.gif',
+            width:  16,
+            height: 16,
+            alt:    '[emoji:' + code + ']',
+            title:  '[emoji:' + code + ']',
+            class:  'emoji emoji-google',
+        }, style, event );
+    }
+
+    function EmojiTable( toolbar, target ) {
+        var obj = this;
+
+        this.opened = false;
+        this.table  = createElement( 'div', {}, {
+            textAlign: 'left',
+        } );
+
+        var emojiList = getEmojiList();
+        for( var i = 0; i < emojiList.length; ++i ) {
+            var emoji = emojiImage( emojiList[i], {}, {
+                click: function (e) { obj.insertEmoji( e, this ); },
+            } );
+            this.table.appendChild( emoji );
+        }
+
+        toolbar.appendChild( emojiImage( '330', null, {
+            click: function( e ){ obj.onclick( e ); },
+        } ) );
+
+        this.open = function() {
+            toolbar.appendChild( this.table );
+            this.opened = true;
+        };
+
+        this.close = function() {
+            toolbar.removeChild( this.table );
+            this.opened = false;
+        };
+
+        this.insertEmoji = function( e, emoji ) {
+            var value  = target.value;
+            var start  = target.selectionStart;
+            var end    = target.selectionEnd;
+            var before = value.slice( 0, start );
+            var after  = value.slice( end );
+
+            target.value = before + emoji.alt + after;
+        };
+
+        this.onclick = function( e ) {
+            if( this.opened ) this.close();
+            else              this.open();
+        };
+    }
+
+    function getEmojiList() {
+        return [
+            '000', '001', '002', '003', '004', '005', '006', '007', '008', '011',
+            '012', '013', '014', '015', '018', '019', '01A', '01B', '01D', '02A',
+            '02B', '02C', '02D', '02E', '02F', '030', '031', '032', '033', '034',
+            '035', '036', '038', '03C', '03D', '03E', '03F', '040', '050', '051',
+            '190', '191', '195', '19A', '1B7', '1B8', '1B9', '1BA', '1BC', '1BD',
+            '1BE', '1BF', '320', '321', '322', '323', '324', '325', '326', '327',
+            '329', '32B', '32C', '330', '331', '332', '335', '339', '33A', '33D',
+            '33E', '33F', '340', '349', '4B0', '4B2', '4B3', '4B4', '4B5', '4B6',
+            '4B7', '4B9', '4BA', '4C3', '4C9', '4CD', '4CE', '4CF', '4D0', '4D1',
+            '4D6', '4DC', '4DD', '4E2', '4EF', '4F0', '4F1', '4F2', '4F3', '506',
+            '50F', '510', '511', '512', '522', '523', '525', '526', '527', '528',
+            '529', '52B', '536', '537', '538', '539', '53A', '53E', '546', '553',
+            '7D0', '7D1', '7D2', '7D3', '7D4', '7D5', '7D6', '7D7', '7D8', '7D9',
+            '7DF', '7E0', '7E2', '7E4', '7E5', '7E6', '7E8', '7E9', '7EA', '7EB',
+            '7F5', '7F6', '7F7', '7FA', '7FC', '800', '801', '803', '804', '805',
+            '806', '807', '808', '80A', '813', '814', '81C', '81D', '823', '824',
+            '825', '82B', '82C', '82D', '82E', '82F', '830', '831', '832', '833',
+            '834', '835', '836', '837', '960', '961', '962', '963', '964', '980',
+            '981', '982', '983', '984', '985', '986', 'AF0', 'AF1', 'AF2', 'AF3',
+            'AF4', 'AF5', 'AF6', 'AF7', 'B04', 'B05', 'B06', 'B07', 'B08', 'B0C',
+            'B0D', 'B0E', 'B0F', 'B1A', 'B1B', 'B1C', 'B1D', 'B1E', 'B20', 'B21',
+            'B22', 'B23', 'B27', 'B28', 'B29', 'B2A', 'B2B', 'B2C', 'B2D', 'B2F',
+            'B30', 'B31', 'B36', 'B48', 'B55', 'B56', 'B57', 'B58', 'B59', 'B5B',
+            'B5C', 'B5D', 'B60', 'B61', 'B81', 'B82', 'B83', 'B84', 'B85', 'B93',
+            'B94', 'B95', 'B96', 'B97', 'E10', 'E11', 'E12', 'E13', 'E14', 'E15',
+        ];
     }
 
 })( this.unsafeWindow || window );
